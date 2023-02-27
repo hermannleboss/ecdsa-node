909c240248c6052ea218e56ae42d67d8a1d0274b
Hermann EHO <hermannleboss@gmail.com>
2023-02-27 19:59:10 +0000

    feat: fix load error

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,45 +1,52 @@
 import server from "./server"
 
 import * as secp from "ethereum-cryptography/secp256k1"
-import { toHex } from "ethereum-cryptography/utils"
-import { keccak256 } from "ethereum-cryptography/keccak"
+import {toHex} from "ethereum-cryptography/utils"
+import {keccak256} from "ethereum-cryptography/keccak"
 
 function getAddress(publicKey) {
-  return keccak256(publicKey.slice(1)).slice(-20)
+    return keccak256(publicKey.slice(1)).slice(-20)
 }
 
-function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
-  async function onChange(evt) {
-    const privateKey = evt.target.value
-    setPrivateKey(privateKey)
-    const publicKey = secp.getPublicKey(privateKey)
-    setAddress(toHex(getAddress(publicKey)))
-    console.log("address 0X", address)
-    if (address) {
-      const {
-        data: { balance }
-      } = await server.get(`balance/${address}`)
-      setBalance(balance)
-    } else {
-      setBalance(0)
+function Wallet({address, setAddress, balance, setBalance, privateKey, setPrivateKey}) {
+
+    async function onChange(evt) {
+        const privateKey = evt.target.value
+        setPrivateKey(privateKey)
+        let publicKey = ""
+        setBalance(0)
+        setAddress("")
+        if (privateKey.length === 64) {
+            try {
+                console.log("We have 64 character")
+                publicKey = secp.getPublicKey(privateKey)
+                const newAddress = toHex(getAddress(publicKey))
+                setAddress(newAddress)
+                const {
+                    data: {balance}
+                } = await server.get(`balance/${newAddress}`)
+                setBalance(balance)
+            } catch (e) {
+                console.log(e)
+            }
+        }
     }
-  }
 
-  return (
-    <div className="container wallet">
-      <h1>Your Wallet</h1>
+    return (
+        <div className="container wallet">
+            <h1>Your Wallet</h1>
 
-      <label>
-        Private Key
-        <input placeholder="Type in a private key" value={privateKey} onChange={onChange}></input>
-      </label>
-      {
-        address ? <div> Addresss: 0x{address}</div> : ""
-      }
+            <label>
+                Private Key
+                <input placeholder="Type in a private key" value={privateKey} onChange={onChange}></input>
+            </label>
+            {
+                address ? <div> Addresss: 0x{address}</div> : ""
+            }
 
-      <div className="balance">Balance: {balance}</div>
-    </div>
-  )
+            <div className="balance">Balance: {balance}</div>
+        </div>
+    )
 }
 
 export default Wallet
